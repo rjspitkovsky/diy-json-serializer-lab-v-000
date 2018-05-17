6922104651a5ffadce7216e331b6b137da4f4767
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-17 19:51:53 +0000

    automatically backed up by learn

--- app/serializer/product_serializer.rb
+++ app/serializer/product_serializer.rb
@@ -10,7 +10,7 @@ class ProductSerializer
 
     # the author association can also be represented in JSON
     # serialized_product += '"author": {'
-    serialized_product += '"inventory": "' + product.inventory + '"}'
+    serialized_product += '"inventory": "' + product.inventory.to_s + '"}'
 
     # and end with the close brace
     #serialized_product += '}'

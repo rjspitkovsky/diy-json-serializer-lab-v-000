aa8a0425bd42983dd24bb49ba9f845e649aa4a12
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-17 19:51:28 +0000

    automatically backed up by learn

--- app/serializer/product_serializer.rb
+++ app/serializer/product_serializer.rb
@@ -2,7 +2,7 @@ class ProductSerializer
   def self.serialize(product)
 
       # start with the open brace to create a valid JSON object
-    serialized_post = '{'
+    serialized_product = '{'
 
     serialized_product += '"price": ' + product.price.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
